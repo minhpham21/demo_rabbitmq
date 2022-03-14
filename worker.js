c82992920b9017487ac00f38aa2c7b534d902ae2
root <root@DESKTOP-N2JECLK.localdomain>
2022-03-14 10:02:43 +0700

    demo rabbitmq

---
+++ worker.js
@@ -0,0 +1,31 @@
+var amqp = require('amqplib/callback_api');
+
+
+amqp.connect('amqp://user:password@localhost', function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+        var queue = 'task_queue';
+
+        channel.assertQueue(queue, {
+            durable: true
+        });
+
+        channel.consume(queue, function (msg) {
+            var secs = msg.content.toString().split('.').length - 1;
+
+            console.log(" [x] Received %s", msg.content.toString());
+            setTimeout(function () {
+                console.log(" [x] Done");
+                channel.ack(msg);
+            }, secs * 1000);
+        }, {
+
+            noAck: false
+        });
+    });
+});
